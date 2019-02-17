0091bf52995e319fe3dbbe704b1ddc1c983eca7d
Michał Jamrożek <michal@jamrozek.org>
2019-02-17 19:32:33 +0100

    Added Makefile for easy bumping package version and sending tags to repo

--- build/hookon.umd.js
+++ build/hookon.umd.js
@@ -1,8 +1,8 @@
 /**
   Package: hookon
   Hook your React app on our hooks
-  Version: 0.0.1-alpha.1
-  Build: Sun, 17 Feb 2019 13:58:40 GMT
+  Version: 0.0.1-alpha.2
+  Build: Sun, 17 Feb 2019 18:32:41 GMT
   @license: MIT
   @preserve
 */
